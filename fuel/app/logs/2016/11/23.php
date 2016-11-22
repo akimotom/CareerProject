684b774a2001f6714d4d700eedbcc642df198c99
Miyuki Akimoto <s15014@st.kic.ac.jp>
2016-11-23 01:15:23 +0900

    FuelPHP config.phpの設定

--- fuel/app/logs/2016/11/23.php
+++ fuel/app/logs/2016/11/23.php
@@ -3,3 +3,6 @@
 INFO - 2016-11-23 00:45:47 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "check"
 INFO - 2016-11-23 00:45:47 --> Fuel\Core\Request::execute - Called
 INFO - 2016-11-23 00:45:47 --> Fuel\Core\Request::execute - Setting main Request
+INFO - 2016-11-23 01:13:09 --> Fuel\Core\Request::__construct - Creating a new main Request with URI = "configcheck"
+INFO - 2016-11-23 01:13:09 --> Fuel\Core\Request::execute - Called
+INFO - 2016-11-23 01:13:09 --> Fuel\Core\Request::execute - Setting main Request
